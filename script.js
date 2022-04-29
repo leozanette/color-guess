f05141bca15ffff69de742c995974418d68efca8
Leonardo Zanette <leonardobzanette@gmail.com>
2022-04-29 13:33:12 -0300

    Requisito 4 - DONE

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ function createCirculos(a) {
     }
 }
 createCirculos(6);
+
+
+function changeColors() {
+    let paleta = document.getElementsByClassName('ball');
+    for(i = 0; i <paleta.length; i+=1) {
+        let cor = geraCores()
+        paleta[i].style.backgroundColor = cor
+    }
+}
+changeColors()
+function geraCores() {
+    let r = Math.floor(Math.random()*256);
+    let g = Math.floor(Math.random()*256);
+    let b = Math.floor(Math.random()*256);
+    let rgb = 'rgb('+r+','+g+','+b+')'
+    return rgb;
+}
